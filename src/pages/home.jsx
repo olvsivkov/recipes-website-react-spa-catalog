fd640c046a569f0179bbc7c8a14fd4fd8896ed2c
olvsivkov <1000listov@mail.ru>
2023-06-20 00:13:16 +0300

    finished serch form on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,18 +8,32 @@ function Home() {
   const [catalog, setCatalog] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
 
+  const fetchCategories = () => getAllCategories().then(data => {setCatalog(data.categories)})
+
   useEffect(
     () => {
-      getAllCategories().then(data => {setCatalog(data.categories)})
+      fetchCategories()
     }, []
   )
 
   const handleSubmit = (e) => {
     e.preventDefault()
     let arrayCatalog = []
-    arrayCatalog.push(catalog.find(item => item.strCategory === searchTerm))
-    console.log()
-    setCatalog(arrayCatalog)
+    let searchTermWord 
+    if(searchTerm.length) {
+      searchTermWord = searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1).toLowerCase()
+      arrayCatalog.push(catalog.find(item => item.strCategory === searchTermWord))
+      setCatalog(arrayCatalog)
+      
+    } 
+    else fetchCategories()
+    console.log(catalog)
+  }
+
+  const getValue = (e) => {
+    let word = e.target.value
+    setSearchTerm(word)
+    fetchCategories()
   }
 
   return <>
@@ -27,9 +41,10 @@ function Home() {
         <form onSubmit={handleSubmit} className="find-form search-container">
           <label className="container-1">
             <input
+              placeholder="Search"
               type="text"
               value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={getValue}
             />
           </label>
           <button type="submit" className="btn btn-search container-2">Search</button>
