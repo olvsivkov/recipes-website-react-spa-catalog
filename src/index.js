a5aa3a5efc6c26ccd54c6b0c70ca76482912863e
olvsivkov <1000listov@mail.ru>
2023-06-22 21:55:44 +0300

    fixed router for deploy SPA on the gh-pages part 5

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+
       <App />
-    </HashRouter>
+ 
   </React.StrictMode>
 );
