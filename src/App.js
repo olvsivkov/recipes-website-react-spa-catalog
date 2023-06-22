28b20c0d7ec3652811a7c7132064603bc8719380
olvsivkov <1000listov@mail.ru>
2023-06-22 22:15:00 +0300

    fixed router for deploy SPA on the gh-pages part 7

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import { Layout } from './components/layout';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, BrowserRouter } from 'react-router-dom'; 
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, BrowserRouter, Routes } from 'react-router-dom'; 
 import { Home } from './pages/home';
 import { Category } from './pages/Category';
 import { Recipe } from './pages/Recipe';
 
 const router = createBrowserRouter(createRoutesFromElements(
 <BrowserRouter basename="/recipes-website-react-spa-catalog">
+  <Routes>
     <Route path='/' element={<Layout/>}>
       <Route index element={<Home/>}/>
       <Route path='category/:name' element={<Category/>}/>
       <Route path='meal/:ID' element={<Recipe/>}/>
     </Route>
+  </Routes>
 </BrowserRouter>
 ))
 
